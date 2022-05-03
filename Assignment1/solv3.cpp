479901c06e2876ccc810ae65de99faafedd22318
Aashish-Kumar-saini <72143076+Aashish-Kumar-saini@users.noreply.github.com>
2022-05-03 10:57:36 +0530

    Create solv3.cpp

---
+++ Assignment1/solv3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<iomanip>
+using namespace std;
+
+int main() {
+    
+    int i;
+    long l;
+    char c;
+    float f;
+    double d;
+    
+    cin>>i>>l>>c>>f>>d;
+    cout<<i<<endl<<l<<endl<<c<<endl;
+    cout<<setprecision(8) <<f<<endl;
+    cout<<setprecision(16)<<d;
+    return 0;
+}
