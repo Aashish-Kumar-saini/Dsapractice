66c7eab6e3c9ce2cd0562132b73c00a3682f8d28
Aashish-Kumar-saini <72143076+Aashish-Kumar-saini@users.noreply.github.com>
2022-05-05 10:08:11 +0530

    Create Gp.cpp

---
+++ classwork/Gp.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,n,d;
+    cin>>a>>n>>d;
+    cout<<" your gp"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cout<<a*d<<"\t";
+        a= a*d;
+    }
+}
