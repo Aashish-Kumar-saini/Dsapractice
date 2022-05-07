8f7be08c6a7d975bde239b4b8396c2febcb4e20b
Aashish-Kumar-saini <72143076+Aashish-Kumar-saini@users.noreply.github.com>
2022-05-07 10:28:06 +0530

    Create gcd_recursive.cpp

---
+++ classwork/gcd_recursive.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int gcd(int a,int b)
+{
+    if(a<b) 
+    swap(a,b);
+    if(b==0)
+        return a;
+    return gcd(b,(a%b));
+}
+int main()
+{
+    int a,b,x;
+    cout<<"Enter two no."<<endl;
+    cin>>a>>b;
+    x = gcd(a,b);
+    cout<<x;
+
+}
