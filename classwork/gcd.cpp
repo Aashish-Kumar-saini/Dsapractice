586159e64c5f56b8bab4db426d7779b1146b1611
Aashish-Kumar-saini <72143076+Aashish-Kumar-saini@users.noreply.github.com>
2022-05-07 10:29:02 +0530

    Create gcd.cpp

---
+++ classwork/gcd.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int gcd(int a,int b)
+{
+    if(b>a)
+    {
+        swap(a,b);
+    }
+    int r =a%b;
+    while(r!=0)
+    {
+        a=b;
+        b=r;
+        r = a%b;
+    }
+    return b;
+}
+
+int main()
+{
+    int a,b,x;
+    cout<<"Enter two no."<<endl;
+    cin>>a>>b;
+    x = gcd(a,b);
+    cout<<x;
+
+}
