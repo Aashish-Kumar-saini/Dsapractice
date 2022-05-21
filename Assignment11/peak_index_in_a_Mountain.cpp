9bcf00f6ae8fae81dcfa3a9974d6824ea3c6c599
Aashish-Kumar-saini <72143076+Aashish-Kumar-saini@users.noreply.github.com>
2022-05-21 21:58:24 +0530

    Update peak_index_in_a_Mountain.cpp

--- Assignment11/peak_index_in_a_Mountain.cpp
+++ Assignment11/peak_index_in_a_Mountain.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        int mid,low=0,high=arr.size(),ans=0,H;
+        int mid,low=0,high=arr.size(),ans=0;
         while(low<high)
         {  mid=(low+high)/2;
-            H=arr[mid];
-            if(arr[mid+1]>H)
+            
+            if(arr[mid+1]>arr[mid])
             {
-                H=arr[mid+1];
+                
                 ans = mid+1;
                 low=mid+1;
             }
-         else if(arr[mid-1]>H)
+         else if(arr[mid-1]>arr[mid])
          {
-             H=arr[mid-1];
+           
              ans = mid-1;
              high=mid;
          }
